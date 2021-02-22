28564d343620c8fe9157f8bb924dbc32b225a5f0
guido-lab <guido.xhindoli@gmail.com>
2021-02-22 22:50:12 +0100

    items section created

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,4 @@ class RegistrationForm(UserCreationForm):
     class Meta():
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render ,redirect
 from .forms import RegistrationForm
 from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
