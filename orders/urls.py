9ae36fa6fa0cf540516119a37edce7e13ab70613
guido-lab <guido.xhindoli@gmail.com>
2021-02-22 22:46:09 +0100

    items section created

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 from orders.models import Orders
 from django.urls import path
 from .views import (
-OrderView, HomeListView, CreateOrdersView,
+OrderView, HomeListView, CreateOrdersView, CreateItemsView
 )
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', HomeListView.as_view(), name = 'orders-home'),
     path("orders/<int:pk>/", OrderView.as_view(), name = "orders-detail"),
     path('create_order/', CreateOrdersView.as_view(success_url='/'), name = 'create_entry'),
+    path('create_item/', CreateItemsView.as_view(success_url='/create_order/'), name = 'create_item'),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Orders
+from .models import Items, Orders
 
 
 class OrderView(LoginRequiredMixin, DetailView):
@@ -19,6 +19,15 @@ class CreateOrdersView(LoginRequiredMixin, CreateView):
     model = Orders
     template_name = 'orders/create_order_view.html'
     fields = ['title', 'description', 'item', 'quantity']
+
+    def form_valid(self,form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+class CreateItemsView(LoginRequiredMixin, CreateView):
+    model = Items
+    template_name = 'orders/create_item_view.html'
+    fields = ['name', 'description', 'price']
 
     def form_valid(self,form):
         form.instance.created_by = self.request.user
